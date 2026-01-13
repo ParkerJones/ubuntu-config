9b77f09025f3dad18a7c2273b158ad0cd8d62bb8
Parker Jones <paj027@outlook.com>
2026-01-13 12:58:41 -0500

    Updated config

--- symlink.sh
+++ symlink.sh
@@ -3,3 +3,12 @@ CONFIG_DIR=$HOME/ubuntu-config
 
 rm -rf $HOME/.config/nvim
 ln -sfn $CONFIG_DIR/.config/nvim $HOME/.config/nvim
+
+rm -rf $HOME/.zshrc
+ln -sfn $CONFIG_DIR/.zshrc $HOME/.zshrc
+
+rm -rf $HOME/.config/i3
+ln -sfn $CONFIG_DIR/.config/i3 $HOME/.config/i3
+
+rm -rf $HOME/.ssh/config
+ln -sfn $CONFIG_DIR/.ssh/config $HOME/.ssh/config
